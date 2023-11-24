e37859874694ec2e5843c69875a6b92f2ee6dd8d
Priyanshu761 <priyanshuchaurasiya539@gmail.com>
2023-11-24 08:39:02 +0530

    Add initial files

--- ques3.cpp
+++ ques3.cpp
@@ -1,6 +1,75 @@
-#include<iostream>
+// Vehicle Management System
+
+#include <iostream>
 using namespace std;
-int main(){
-    
+
+class Vehicle
+{
+private:
+    string make, model;
+    int year;
+
+public:
+    void inputDta()
+    {
+        cout << "Enter company Name : " << endl;
+        cin >> make;
+        cout << "Enter Model : " << endl;
+        cin >> model;
+        cout << "Enter Year : " << endl;
+        cin >> year;
+    }
+    void display()
+    {
+        cout << "Make : " << make << endl;
+        cout << "Model : " << model << endl;
+        cout << "Year : " << year << endl;
+    }
+};
+
+class Car : public Vehicle
+{
+private:
+    int numDoor;
+
+public:
+    void inputData()
+    {
+        cout << "Enter the number of doors : " << endl;
+        cin >> numDoor;
+    }
+    void display()
+    {
+        cout << "Doors : " << numDoor << endl;
+    }
+};
+
+class ToyotaCar : public Car
+{
+private:
+    string trimLevel;
+
+public:
+    void inputData()
+    {
+        Vehicle::inputDta();
+        Car::inputData();
+        cout << "Enter the trim level : " << endl;
+        cin >> trimLevel;
+    }
+    void display()
+    {
+        Vehicle::display();
+        Car::display();
+        cout << "Trim Level : " << trimLevel << endl;
+    }
+};
+int main()
+{
+    ToyotaCar myCar;
+    cout << "----------Input Car Info--------------" << endl;
+    myCar.inputData();
+    cout << "----------Output Car Info--------------" << endl;
+    myCar.display();
     return 0;
 }
